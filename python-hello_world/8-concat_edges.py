11ddac47f375126c3ee7d535e9c402e1c1f45a89
root <root@hardcore-franklin.89-147-252-246.plesk.page>
2025-04-26 21:01:13 +0000

    Resolve merge conflictttt116

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming"
-str2 = "Python"
-print(f"{str1} with {str2}")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[80:86] + str[107:113])
