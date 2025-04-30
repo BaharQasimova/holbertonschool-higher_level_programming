1e8a206253005ae98f2284f7663cf90a527dbfb4
BaharQasimova <bahargsmff@gmail.com>
2025-04-30 15:55:41 +0000

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):  # Outer loop for the first digit (0 to 9)
-    for j in range(i + 1, 10):  # Inner loop for the second digit (i+1 to 9)
-        if i != j:  # Ensure that the two digits are different
-            if i == 8 and j == 9:  # Check for the last combination
-                print(f"{i}{j}")
-            else:
-                print(f"{i}{j}", end=", ")
+combinations = []
+for i in range(10): 
+    for j in range(i + 1, 10):
+        combinations.append(f"{i}{j}")
+print(", ".join(combinations))
