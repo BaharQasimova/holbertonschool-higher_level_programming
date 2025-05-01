0cede2645c7924f5af258f97f3ed96ee259f8f38
BaharQasimova <bahargsmff@gmail.com>
2025-05-01 16:30:12 +0000

    1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import calculator_1
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    result_add = calculator_1.add(a, b)
+    result_sub = calculator_1.sub(a, b)
+    result_mul = calculator_1.mul(a, b)
+    result_div = calculator_1.div(a, b)
+    print(f"{a} + {b} = {result_add}")
+    print(f"{a} - {b} = {result_sub}")
+    print(f"{a} * {b} = {result_mul}")
+    print(f"{a} / {b} = {result_div}")
