bfd0ac40d1f38d3343cde61b1d514366ca131347
BaharQasimova <bahargsmff@gmail.com>
2025-05-08 12:14:56 +0000

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,6 @@
+   if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
