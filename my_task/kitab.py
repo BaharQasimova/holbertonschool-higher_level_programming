6858fabfa7de981e2e96b7a6a820145b5be97d90
BaharQasimova <bahargsmff@gmail.com>
2025-07-24 19:37:29 +0400

    kitabxana

--- my_task/kitab.py
+++ my_task/kitab.py
@@ -49,3 +49,42 @@ class Library:
                     print(f" '{book.title}' kitabi kiraye verilmeyib.")
                     return
         print(f" '{title}' kitab kitabxanada tapilmadi.")
+        
+    def main():
+       library = Library("Ağıllı Kitabxana")
+    
+    while True:
+        print("\nAğıllı Kitabxanaya xoş gelmisiz.")
+        print("1. Kitab elave et.")
+        print("2. Kitabları goster.")
+        print("3. Kitab kiraye gotur.")
+        print("4. Kitabı qaytar.")
+        print("5. Çıxış")
+        
+        choice = input("Seçiminizi daxil edin (1-5): ")
+        
+        if choice == '1':
+            title = input("Kitabın adını daxil edin: ")
+            author = input("Kitabın muellifini daxil edin: ")
+            library.add_book(title, author)
+        
+        elif choice == '2':
+            library.display_books()
+        
+        elif choice == '3':
+            title = input("Kiraye goturmek istediyiniz kitabın adını yazın: ")
+            library.borrow_book(title)
+        
+        elif choice == '4':
+            title = input("Qaytarmaq istediyiniz kitabın adını yazın: ")
+            library.return_book(title)
+        
+        elif choice == '5':
+            print("Sag olun, goruserik!")
+            break
+        
+        else:
+            print("Yanlış seçim. Zehmet olmasa 1-den 5-e qeder reqem daxil edin.")
+
+if __name__ == "__main__":
+    main()
