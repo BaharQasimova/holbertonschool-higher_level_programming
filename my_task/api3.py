707777f63339f60a7025f641d370212f7eaf424e
BaharQasimova <bahargsmff@gmail.com>
2025-08-14 15:58:08 +0400

    api

--- my_task/api3.py
+++ my_task/api3.py
@@ -12,5 +12,5 @@ def info():
     """
     return html_text
 
-if __name__ == '__main__':
-    app.run(debug=True, port=4004)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0",port=4050)
